d78ef1c833b2bf618f89c88db5f8a7ed8db30a1b
Mihalcea Andreea <andreea_mih97@yahoo.com>
2022-05-04 18:02:17 +0300

    Adding the messaging feature

---
+++ API/Data/Migrations/20220428170915_MessagesEntityRefactor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class MessagesEntityRefactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MesageSent",
+                table: "Messages",
+                newName: "MessageSent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MessageSent",
+                table: "Messages",
+                newName: "MesageSent");
+        }
+    }
+}
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -20,7 +21,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent },
-      { path: 'members/:username', component: MemberDetailComponent },
+      {
+        path: 'members/:username',
+        component: MemberDetailComponent,
+        resolve: { member: MemberDetailedResolver },
+      },
       {
         path: 'member/edit',
         component: MemberEditComponent,
